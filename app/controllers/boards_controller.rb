eb54698a609866a1f261019773593259ead2de64
excavator <str_token@mail.ru>
2019-09-13 19:11:52 +0300

    add and config devise for login users

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_board, only: [:show, :edit, :update, :destroy]
 
   # GET /boards

3b7b8bfb18a86f4f63f843b2f67b9e2ecf953aca
excavator <str_token@mail.ru>
2019-09-18 21:18:28 +0300

    add nav panel bootstrap

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,10 +1,10 @@
 class Pin < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
-
+  # behave
   belongs_to :board
-
+  # rmagik to MagickImage
   mount_uploader :image, ImageUploader
-
+  # gem 'kaminari'
   paginates_per 10
 end
